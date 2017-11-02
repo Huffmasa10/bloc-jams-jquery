b194b57353a108f11e7b38a06250d6b513f3dd9e
Seth Huffman <huffman.seth@gmail.com>
2017-11-01 22:11:44 -0400

    updated master

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 $( document ).ready(function() {
-   $('#album-title').text(album.title);
-   $('img#album-cover-art').attr('src', album.albumArtUrl);
-   $('.artist').text(album.artist);
-   $('#release-info').text(album.releaseInfo);
+$('#album-title').text(album.title);
+$('img#album-cover-art').attr('src', album.albumArtUrl);
+$('.artist').text(album.artist);
+$('#release-info').text(album.releaseInfo);
  });
-=======
-$(document).ready(function() {
-  $('#album-title').text(album.title);
-  $('img#album-cover-art').attr('src', album.albumArtUrl);
-});
->>>>>>> checkpoint-11
-=======
-$(document).ready(function() {
-  $('#album-title').text(album.title);
-  $('img#album-cover-art').attr('src', album.albumArtUrl);
-  $('.artist').text(album.artist);
-  $('#release-info').text(album.releaseInfo);
-});
->>>>>>> Assignment-11
