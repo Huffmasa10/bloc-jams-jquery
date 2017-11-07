b59530ea7016a37062eaa860bc9f6777d075f3cf
Seth Huffman <huffman.seth@gmail.com>
2017-11-06 19:02:40 -0500

    assignment-14

--- scripts/player.js
+++ scripts/player.js
@@ -6,6 +6,16 @@ class Player {
     this.soundObject = new buzz.sound(this.currentlyPlaying.soundFileUrl);
   }
 
+  prettyTime(timeInSeconds) {
+    const m = Math.floor((timeInSeconds % 3600) / 60);
+    const s = Math.floor(timeInSeconds % 60);
+    return [
+    m < 1 ? '0' : m,
+    s > 9 ? s : '0' + s,
+  ].join(":");
+  }
+
+
   getDuration() {
     return this.soundObject.getDuration();
   }
@@ -13,14 +23,14 @@ class Player {
   getTime() {
     return this.soundObject.getTime();
   }
-  
+
   playPause (song = this.currentlyPlaying) {
     if (this.currentlyPlaying !== song) {
       // Stop the currently playing sound file (even if nothing is playing)
       this.soundObject.stop();
       // Clear classes on the song that's currently playing
       this.currentlyPlaying.element.removeClass('playing paused');
-      
+
       // Update our currentlyPlaying and playState properties
       this.currentlyPlaying = song;
       this.playState = 'stopped';
@@ -37,12 +47,12 @@ class Player {
       this.currentlyPlaying.element.removeClass('playing').addClass('paused');
     }
   }
-  
+
   skipTo (percent) {
     if (this.playState !== 'playing') { return }
     this.soundObject.setTime( (percent / 100) * this.soundObject.getDuration() );
   }
-  
+
   setVolume (percent) {
     this.volume = percent;
     this.soundObject.setVolume(percent);
@@ -50,4 +60,3 @@ class Player {
 }
 
 const player = new Player();
-
